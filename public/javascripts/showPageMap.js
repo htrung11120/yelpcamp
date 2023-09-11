32d5d201c71e3f8d214e7e8250a05b9e61f3a38b
htrung11120 <htrung11120@gmail.com>
2023-09-10 23:11:08 -0400

    add map popup

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -3,11 +3,17 @@ const campgroundLocation = JSON.parse(campground);
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: campgroundLocation.geometry.coordinates,
-    zoom: 7
+    style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
+    center: campgroundLocation.geometry.coordinates, // starting position [lng, lat]
+    zoom: 10 // starting zoom
 });
 
-const marker = new mapboxgl.Marker()
+new mapboxgl.Marker()
     .setLngLat(campgroundLocation.geometry.coordinates)
-    .addTo(map);
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${campgroundLocation.title}</h3><p>${campgroundLocation.location}</p>`
+            )
+    )
+    .addTo(map)
