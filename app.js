6eb988fea5c78d43f28a7b93772db6ce2483632a
htrung11120 <htrung11120@gmail.com>
2023-09-12 23:38:07 -0400

    finish

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const flash = require('connect-flash')
 const passport = require('passport');
 const LocalStrategy = require('passport-local')
@@ -22,13 +23,14 @@ const campgroundsRouter = require('./routes/campgrounds.js')
 const reviewsRouter = require('./routes/reviews')
 const userRouter = require('./routes/user')
 const mongoSanitize = require('express-mongo-sanitize');
-
+const helmet = require('helmet')
+const dbUrl = process.env.DB_URL
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
+db.once("open", () => { 
     console.log("Database connected");
 });
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+mongoose.connect(dbUrl);
 
 
 app.engine('ejs', ejsMate);
@@ -38,7 +40,65 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
+app.use(helmet());
+
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/drq8bsinw/",
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+const store = new MongoStore({
+    url: dbUrl,
+    crypto: {
+        secret: 'thisshouldbeabettersecret!'
+    },
+    touchAfter: 24 * 3600
+})
+
+store.on('error', function (e) {
+    console.log('session error')
+})
 const sessionConfig = {
+    store,
     name: 'session',
     secret: 'thishouldbeagoodsecret',
     resave: false,
